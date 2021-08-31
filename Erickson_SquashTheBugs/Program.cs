1013c5d83af61a17d90be37e9c4b0fff3a4ba05d
zachcerickson@outlook.com <zachcerickson@outlook.com>
2021-08-31 17:32:57 -0400

    Updated PS2

--- Erickson_SquashTheBugs/Program.cs
+++ Erickson_SquashTheBugs/Program.cs
@@ -18,21 +18,23 @@ namespace SquashTheBugs
         {
             // declare int counter
             // int i = 0
-            // requires a semicolon
+            // syntax error: requires a semicolon
             int i = 0;
 
             // declare string to hold all numbers
-            // must be outside for loop so info can be stored and not rewritten each iteration
-            // of the loop
+            // logical error: must be outside for loop so info can be stored and
+            // not rewritten each iteration of the loop
             string allNumbers = null;
+
 
             // loop through the numbers 1 through 10
             // for (i = 1; i < 10; ++i)
-            // increment should be after variable, and operand should be '<=' so that
-            // the loop stops after number 10 is processed
+            // increment should be after variable (syntax error), and operand should be '<=' so that
+            // the loop stops after number 10 is processed (logical error)
             for (i = 1; i <= 10; i++)
             {
-                // Must account for undefined case where i = 1
+                // runtime error: must account for undefined case where i = 1
+                // so that we do not divide by 0
                 if (i == 1)
                 {
                     // output explanation of calculation
@@ -48,13 +50,14 @@ namespace SquashTheBugs
                 {
                     // output explanation of calculation
                     // Console.Write(i + "/" + i - 1 + " = ");
-                    // need parentheses around 'i-1'
+                    // logical error: need parentheses around 'i-1'
                     Console.Write(i + "/" + (i - 1) + " = ");
 
                     // output the calculation based on the numbers
 
-                    //Console.WriteLine(i / (i - 1));
-                    // must convert integer to float so calculation can account for fractions
+                    // Console.WriteLine(i / (i - 1));
+                    // logical error
+                    // must convert integer to double so calculation can account for fractions
                     float myFloat = (float)i;
                     Console.WriteLine(myFloat / (myFloat - 1));
 
@@ -63,6 +66,7 @@ namespace SquashTheBugs
 
                     // increment the counter
                     // i = i + 1;
+                    // logical error
                     // unnecessary since the loop already increments after each iteration.
                 }
 
@@ -70,6 +74,7 @@ namespace SquashTheBugs
 
             // output all numbers which have been processed
             // Console.WriteLine("These numbers have been processed: " allNumbers);
+            // syntax error
             // needed to add a plus operator
             Console.WriteLine("These numbers have been processed: " + allNumbers);
         }
