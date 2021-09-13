7de56f588b25ba57733dd511e631675f1bcbd1d6
zachcerickson@outlook.com <zachcerickson@outlook.com>
2021-09-12 23:17:06 -0400

    Problem Set 8 question 9

---
+++ Problem_Set_8_Question_9/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_Set_8_Question_9
+{
+    // Class: Program
+    // Author: Zachary Erickson
+    // Purpose: Problem Set 8 Question 9
+    // Restrictions: None
+
+    class Program
+    {
+        // Method: Main
+        // Purpose: Add double quotes around every word in a string
+        // Restrictions: None
+
+        static void Main(string[] args)
+        {
+            // initialize strings for user input and the result
+            string userInput = null;
+            string resultString = "";
+
+
+            // make sure the user inputted something
+            do
+            {
+                Console.WriteLine("Enter a string:");
+                userInput = Console.ReadLine();
+
+            } while (userInput.Length == 0);
+
+
+            // split the string into an array of word strings
+            string[] words = userInput.Split(' ');
+
+            // go through each word in the array
+            foreach (string word in words)
+            {
+                // adds double quotes to each word and adds them to result string
+                resultString = resultString + "\"" + word +  "\"" + " ";
+
+            }
+
+            // return result string
+            Console.WriteLine("Your result string is: " + resultString);
+
+
+        }
+    }
+}
