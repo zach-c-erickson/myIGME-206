1334ca521a03a44f764a7d6a137a3b0c1404ead1
zach-c-erickson <zce1382@g.rit.edu>
2021-09-23 23:25:37 -0400

    Unit test 1 question 12

---
+++ Unit Test Problem 4/Program.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Timers;
+
+namespace Unit_Test_Problem_4
+{
+    // Class: Program
+    // Author: Zachary Erickson
+    // Purpose: Recreate the 3 questions program
+    // Restrictions: None
+    class Program
+    {
+        // bTimeOut boolean
+        static bool bTimeOut = false;
+
+        // timeOutTimer Timer
+        static Timer timeOutTimer;
+
+        // string and int for response
+        static string sResponse = "";
+
+        // question 1 and answer
+        static string question1 = "What is your favorite color?";
+        static string answer1 = "black";
+
+        // question 2 and answer
+        static string question2 = "What is the answer to life, the universe and everything?";
+        static string answer2 = "42";
+
+        // question 3 and answer
+        static string question3 = "What is the airspeed velocity of an unladen swallow?";
+        static string answer3 = "What do you mean? African or European Swallow?";
+
+        // Method: Main
+        // Purpose: Prompt the user for one of three questions. Give
+        // them 5 seconds to answer the questions
+        // Restrictions: None
+        static void Main(string[] args)
+        {
+            // string and int of user question choice
+            string sChoice = "";
+            
+            
+
+            // boolean for checking valid input
+            bool bValid = false;
+
+            // play again?
+            string sAgain = "";
+
+           
+
+
+        start:
+
+            // ask for user's question choice
+            Console.Write("Choose your question(1-3): ");
+            
+
+            // save choice
+            sChoice = Console.ReadLine();
+
+            bValid = false;
+
+            Console.WriteLine();
+
+            timeOutTimer = new Timer(5000);
+
+            timeOutTimer.Elapsed += new ElapsedEventHandler(TimesUp);
+
+            bTimeOut = false;
+           
+            switch (sChoice)
+            {
+
+                case "1":
+                 
+                    timeOutTimer.Start();
+                    Console.WriteLine("You have 5 seconds to answer the following question:");
+                    Console.WriteLine(question1);
+                        
+                    sResponse = Console.ReadLine();
+                        
+                    timeOutTimer.Stop();
+                     
+                    bValid = true;
+
+                    if(sResponse.ToLower() == answer1 && !bTimeOut)
+                    {
+                        Console.WriteLine("Well done!");
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Wrong! The answer is: " + answer1);
+                        break;
+                    }
+                    
+                    
+                case "2":
+                    timeOutTimer.Start();
+                    Console.WriteLine("You have 5 seconds to answer the following question:");
+                    Console.WriteLine(question2);
+                    sResponse = Console.ReadLine();
+                    timeOutTimer.Stop();
+
+                    if (sResponse.ToLower() == answer2 && !bTimeOut)
+                    {
+                        Console.WriteLine("Well done!");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Wrong! The answer is: "+ answer2);
+                    }
+                    break;
+                case "3":
+                    timeOutTimer.Start();
+                    Console.WriteLine("You have 5 seconds to answer the following question:");
+                    Console.WriteLine(question3);
+                    sResponse = Console.ReadLine();
+                    timeOutTimer.Stop();
+
+                    if (sResponse.ToLower() == answer3.ToLower() && !bTimeOut)
+                    {
+                        Console.WriteLine("Well done!");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Wrong! The answer is: " + answer3);
+                    }
+                    break;
+                default:
+                    goto start;
+            }
+            do
+            {
+                Console.Write("Play again?");
+                sAgain = Console.ReadLine();
+
+                switch (sAgain.Substring(0, 1).ToLower())
+                {
+                    case "y":
+                        bValid = true;
+                        goto start;
+                    case "n":
+                        bValid = true;
+                        break;
+                    default:
+                        break;
+                }
+            } while (!bValid);
+            
+        }
+
+        static void TimesUp(object source, ElapsedEventArgs e)
+        {
+            Console.WriteLine();
+            Console.WriteLine("Time's up!");
+            Console.WriteLine("Please press enter");
+            bTimeOut = true;
+
+            timeOutTimer.Stop();
+        }
+    }
+}
---
+++ Unit_Test_Question_12/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unit_Test_Question_12
+{
+
+    // Class: Program
+    // Author: Zachary Erickson
+    // Purpose: Unit test question 12
+    // Restrictions: None
+    class Program
+    {
+
+        // Method: Main
+        // Purpose: Prompt user for name and give a raise if their name is Zach
+        // Restrictions: None
+        static void Main(string[] args)
+        {
+            // save string for user input
+            string sName;
+
+            // double for salary
+            double dSalary = 30000;
+
+            // prompt for user name and save as sName
+            Console.Write("What is your name? : ");
+            sName = Console.ReadLine();
+
+            // call GiveRaise method and if true, congratulate and display new salary
+            if (GiveRaise(sName, ref dSalary))
+            {
+                Console.WriteLine("Congratulations! Your new salary is {0}", dSalary);
+            }
+            else { Console.WriteLine("No raise for you! Your salary is {0}", dSalary); }
+
+
+        }
+
+
+        // Method: GiveRaise
+        // Purpose: If name is zach, add 19999.99 to salary and return true,
+        //          else, return false
+        // Restrictions: None
+        static bool GiveRaise(string name, ref double salary)
+        {
+            if (name.ToLower() == "zach")
+            {
+                salary += 19999.99;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
