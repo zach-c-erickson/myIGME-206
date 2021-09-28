6c4a6324b933acc25e91b42e0707b13f90712487
zach-c-erickson <zce1382@g.rit.edu>
2021-09-27 21:11:12 -0400

    Problem set 11 question 5

---
+++ Vehicles/Class1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vehicles
+{
+    public abstract class Vehicle
+    {
+        public virtual void LoadPassenger()
+        {
+
+        }
+    }
+
+    public interface IPassengerCarrier
+    {
+        void LoadPassenger();
+    }
+
+    public interface IHeavyLoadCarrier
+    {
+
+    }
+
+    public abstract class Car : Vehicle
+    {
+
+    }
+
+    public abstract class Train : Vehicle
+    {
+
+    }
+
+    public class Compact : Car, IPassengerCarrier
+    {
+
+    }
+
+    public class SUV : Car, IPassengerCarrier
+    {
+
+
+    }
+
+    public class Pickup: Car, IPassengerCarrier, IHeavyLoadCarrier
+    {
+
+    }
+
+    public class PassengerTrain: Train, IPassengerCarrier
+    {
+
+    }
+
+    public class FreightTrain: Train, IHeavyLoadCarrier
+    {
+
+    }
+
+    public class _424DoubleBogey : Train, IHeavyLoadCarrier
+    {
+
+    }
+
+
+}
