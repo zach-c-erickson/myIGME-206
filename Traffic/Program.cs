9649b91715ca3f5d3ee0819c476815cb45691c65
zach-c-erickson <zce1382@g.rit.edu>
2021-09-27 22:55:29 -0400

    Problem Set 11 question 5

---
+++ Traffic/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vehicles;
+
+
+namespace Traffic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Compact myCompact = new Compact();
+            FreightTrain myFreightTrain = new FreightTrain();
+            SUV mySUV = new SUV();
+
+            AddPassenger(myCompact);
+            AddPassenger(mySUV);
+            AddPassenger(myFreightTrain);
+        }
+
+        public static void AddPassenger(object obj)
+        {
+            IPassengerCarrier passengerCarrier;
+
+            passengerCarrier = (IPassengerCarrier)obj;
+
+            passengerCarrier.LoadPassenger();
+
+            
+                Console.WriteLine(obj.ToString());
+            
+            
+        }
+    }
+}
--- Vehicles/Class1.cs
+++ Vehicles/Class1.cs
@@ -4,8 +4,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
+// Class Library: Vehicles
+// Author: Zachary Erickson
+// Purpose: Create a library for vehicles
+// Restrictions: None
 namespace Vehicles
 {
+
+    //[+A:Vehicle||+LoadPassenger():v]
     public abstract class Vehicle
     {
         public virtual void LoadPassenger()
@@ -13,53 +20,56 @@ namespace Vehicles
 
         }
     }
-
+    //[+I:IPassengerCarrier||LoadPassenger()]
     public interface IPassengerCarrier
     {
         void LoadPassenger();
     }
 
+    //[+I:HeavyLoadCarrier||]
     public interface IHeavyLoadCarrier
     {
 
     }
-
+    //[+A:Car||]
     public abstract class Car : Vehicle
     {
 
     }
 
+    //[+A:Train||]
     public abstract class Train : Vehicle
     {
 
     }
 
+    //[+Compact||]
     public class Compact : Car, IPassengerCarrier
     {
 
     }
-
+    //[+SUV||]
     public class SUV : Car, IPassengerCarrier
     {
 
 
     }
-
+    //[+Pickup||]
     public class Pickup: Car, IPassengerCarrier, IHeavyLoadCarrier
     {
 
     }
-
+    //[+PassengerTrain||]
     public class PassengerTrain: Train, IPassengerCarrier
     {
 
     }
-
+    //[+FreightTrain||]
     public class FreightTrain: Train, IHeavyLoadCarrier
     {
 
     }
-
+    //[+_424DoubleBogey||]
     public class _424DoubleBogey : Train, IHeavyLoadCarrier
     {
 
