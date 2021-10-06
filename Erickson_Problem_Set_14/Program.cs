1c9fd222319c82f91d24fe583e4ac31e9f0dc718
zach-c-erickson <zce1382@g.rit.edu>
2021-10-06 18:20:54 -0400

    Problem Set 14

---
+++ Erickson_Problem_Set_14/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Erickson_Problem_Set_14
+{
+    // Class: Program
+    // Author: Zachary Erickson
+    // Purpose: Problem Set 14
+    // Restrictions: None
+    class Program
+    {
+        // Method: Main
+        // Purpose: Create an instance of Baby and Adult and call MyMethod on both
+        static void Main(string[] args)
+        {
+            Baby myBaby = new Baby();
+            Adult myAdult = new Adult();
+
+            MyMethod(myBaby);
+            MyMethod(myAdult);
+        }
+
+        // Method: MyMethod
+        // Purpose: Using an object as a parameter, create an ITalk, cast the object to ITalk and call ITalk.Talk().
+        public static void MyMethod(object myObject)
+        {
+            ITalk iTalk = null;
+
+            iTalk = (ITalk)myObject;
+
+            iTalk.Talk();
+            
+        }
+    }
+
+
+    
+    // Interface: ITalk
+    // Purpose: Contains Talk() method
+    public interface ITalk
+    {
+        void Talk();
+    }
+
+    // Class: Baby
+    // Purpose: Inherits from ITalk and creates the Baby.Talk() method
+    public class Baby : ITalk
+    {
+        public void Talk()
+        {
+            Console.WriteLine("Goo goo ga ga!");
+        }
+    }
+
+    // Class: Adult
+    // Purpose: Inherits from ITalk and creates the Adult.Talk() method
+    public class Adult : ITalk
+    {
+        public void Talk()
+        {
+            Console.WriteLine("Hello there, friend!");
+        }
+    }
+}
---
+++ Erickson_Problem_Set_14/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Erickson_Problem_Set_14")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Erickson_Problem_Set_14")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("6f31ace6-581c-44b0-85d5-57fe54f0d67c")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
