23c3e8fd3ee8bd67e06f09dd74b4a2ac2a5e41d8
zach-c-erickson <zce1382@g.rit.edu>
2021-10-22 20:15:42 -0400

    Unit_test_question_10

---
+++ Unit_Test_Question_10/Program.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unit_Test_Question_10
+{
+    // Class: Program
+    // Author: Zachary Erickson
+    // Purpose: Unit Test Question 10
+    class Program
+    {
+        // Method: Main
+        // Purpose: Create a Bloodhound and Valkyrie object and call MyMethod on both
+        static void Main(string[] args)
+        {
+            Bloodhound legend1 = new Bloodhound();
+            Valkyrie legend2 = new Valkyrie();
+
+            MyMethod(legend1);
+            MyMethod(legend2);
+        }
+
+        // Method: MyMethod
+        // Purpose: Takes an object as a parameter and calls methods associated with the object
+        static void MyMethod(object obj)
+        {
+            // Cast object as a Legend
+            ReconLegend reconLegend = (ReconLegend)obj;
+
+            // Cast object as an ILegend
+            ILegend iLegend = (ILegend)obj;
+
+            // Cast object as an IEmote
+            IEmote iEmote = (IEmote)obj;
+
+            // Call methods associated with the abstract class ReconLegend
+            reconLegend.Shoot();
+            reconLegend.Jump();
+
+            // Call methods associated with ILegend
+            iLegend.Passive();
+            iLegend.Tactical();
+            iLegend.Ultimate();
+
+            // Call method associated with IEmote
+            iEmote.Emote();
+            
+        }
+    }
+
+    // [+I:ILegend| Tactical(); Ultimate(); Passive()]
+    public interface ILegend
+    {
+        void Tactical();
+        void Ultimate();
+        void Passive();
+    }
+
+    // [+I:IEmote| Emote()]
+    public interface IEmote
+    {
+        void Emote();     
+    }
+
+    // [+A:ReconLegend| -legendType:string| +LegendType:r; +Shoot():a; +Jump():v| +(myString:string) ]
+    public abstract class ReconLegend
+    {
+        private string legendType = "Recon";
+
+        public string LegendType
+        {
+            get { return legendType; }
+            
+        }
+        public abstract void Shoot();
+        public virtual void Jump()
+        {
+            Console.WriteLine("Jump in air");
+        }
+
+        
+    }
+
+    // [+Bloodhound| -name:string | +name:string:r; +Tactical(); +Ultimate(); +Passive();
+    // +Emote(); +Shoot():o]
+    // [+A:ReconLegend] <-.- [+Bloodhound]
+    // [+I:ILegend] ^ [+Bloodhound]
+    // [+I:IEmote] ^ [+Bloodhound]
+    public class Bloodhound : ReconLegend, ILegend, IEmote
+    {
+        private string name = "Bloodhound";
+       
+
+        public string Name
+        {
+            get { return name; }
+        }
+        
+
+        public void Tactical()
+        {
+            Console.WriteLine("Scanning for enemies...");
+        }
+
+        public void Ultimate()
+        {
+            Console.WriteLine("Entering Beast of the Hunt...");
+        }
+
+        public void Passive()
+        {
+            Console.WriteLine("Tracking Enemies...");
+        }
+
+        public  void Emote()
+        {
+            Console.WriteLine("Smell the dirt...");
+        }
+        public override void Shoot()
+        {
+            Console.WriteLine("Fire!");
+        }
+      
+       
+
+    }
+    // [+Valkyrie| -name:string | +name:string:r; +Tactical(); +Ultimate(); +Passive();
+    // +Emote(); +Shoot():o; +Jump():o]
+    // [+A:ReconLegend] <-.- [+Valkyrie]
+    // [+I:ILegend] ^ [+Valkyrie]
+    // [+I:IEmote] ^ [+Valkyrie]
+    public class Valkyrie : ReconLegend, ILegend, IEmote
+    {
+        private string name = "Valkyrie";
+        
+        public string Name
+        {
+            get { return name; }
+        }
+
+        public void Tactical()
+        {
+            Console.WriteLine("Launching Barrage...");
+        }
+
+        public void Ultimate()
+        {
+            Console.WriteLine("Taking Off...");
+        }
+
+        public void Passive()
+        {
+            Console.WriteLine("Scan Beacon...");
+        }
+        public void Emote()
+        {
+            Console.WriteLine("Do a barrel roll!...");
+        }
+        public override void Shoot()
+        {
+            Console.WriteLine("Fire!");
+        }
+       
+        public override void Jump()
+        {
+            Console.WriteLine("Hold spacebar to fly...");
+        }
+        
+    }
+}
---
+++ Unit_Test_Question_7/Program.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unit_Test_Question_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tardis myTardis = new Tardis();
+            PhoneBooth myPhoneBooth = new PhoneBooth();
+
+            UsePhone(myTardis);
+            UsePhone(myPhoneBooth);
+        }
+        static void UsePhone(object obj)
+        {
+            PhoneInterface myPhoneInterface = null;
+
+            myPhoneInterface = (PhoneInterface)obj;
+
+            myPhoneInterface.MakeCall();
+            myPhoneInterface.HangUp();
+
+            if(obj.GetType() == typeof(Tardis))
+            {
+                Tardis obj1 = new Tardis();
+                obj1.TimeTravel();
+            }
+            if(obj.GetType() == typeof(PhoneBooth))
+            {
+                PhoneBooth obj2 = new PhoneBooth();
+                obj2.OpenDoor();
+            }
+        }
+    }
+
+   
+
+
+    public interface PhoneInterface
+    {
+        void Answer();
+        void MakeCall();
+        void HangUp();
+
+    }
+
+    public abstract class Phone
+    {
+        private string phoneNumber;
+        public string address;
+
+        public string PhoneNumber;
+
+        public abstract void Connect();
+
+        public abstract void Disconnect();
+
+
+    }
+
+    public class RotaryPhone: Phone, PhoneInterface
+    {
+        public void Answer() { }
+       
+
+        public void MakeCall() 
+        {
+            Console.WriteLine("Dialing...");
+        }
+     
+
+        public void HangUp() 
+        {
+            Console.WriteLine("Call ended...");
+        }
+    
+
+        public override void Connect() { }
+     
+
+        public override void Disconnect() { }
+        
+
+    }
+
+    public class Tardis : RotaryPhone
+    {
+        private bool sonicScrewdriver;
+
+        private byte whichDrWho;
+
+        private string femaleSideKick;
+         
+        public double exteriorSurfaceArea;
+
+        public double interiorVolume;
+
+        public byte WhichDrWho
+        {
+            get { return whichDrWho; }
+        }
+
+        public string FemaleSideKick
+        {
+            get { return femaleSideKick; }
+        }
+
+        public void TimeTravel() 
+        {
+            Console.WriteLine("Time Traveling...");
+        }
+
+        public static bool operator == (Tardis tardis1, Tardis tardis2)
+        {
+            return (tardis1.WhichDrWho == tardis2.WhichDrWho);
+        }
+        public static bool operator != (Tardis tardis1, Tardis tardis2)
+        {
+            return (tardis1.WhichDrWho != tardis2.WhichDrWho);
+        }
+        public static bool operator < (Tardis tardis1, Tardis tardis2)
+        {
+           if (tardis1.WhichDrWho == 10)
+            {
+                return false;
+            }
+           else if (tardis2.WhichDrWho == 10)
+            {
+                return true;
+            }
+            else
+            {
+                return (tardis1.WhichDrWho < tardis2.WhichDrWho);
+            }
+     
+        }
+        public static bool operator > (Tardis tardis1, Tardis tardis2)
+        {
+            if (tardis2.WhichDrWho == 10)
+            {
+                return false;
+            }
+            else if (tardis1.WhichDrWho == 10)
+            {
+                return true;
+            }
+            else
+            {
+                return (tardis1.WhichDrWho > tardis2.WhichDrWho);
+            }
+
+        }
+        public static bool operator <= (Tardis tardis1, Tardis tardis2)
+        {
+            if (tardis1.WhichDrWho == 10 && tardis2.WhichDrWho != 10)
+            {
+                return false;
+            }
+            else if (tardis2.WhichDrWho == 10 && tardis1.WhichDrWho != 10)
+            {
+                return true;
+            }
+            else
+            {
+                return (tardis1.WhichDrWho <= tardis2.WhichDrWho);
+            }
+
+        }
+        public static bool operator >= (Tardis tardis1, Tardis tardis2)
+        {
+            if (tardis1.WhichDrWho == 10 && tardis2.WhichDrWho != 10)
+            {
+                return true;
+            }
+            else if (tardis2.WhichDrWho == 10 && tardis1.WhichDrWho != 10)
+            {
+                return false;
+            }
+            else
+            {
+                return (tardis1.WhichDrWho <= tardis2.WhichDrWho);
+            }
+
+        }
+
+    }
+
+    public class PushButtonPhone:Phone, PhoneInterface
+    {
+        public void Answer() { }
+        public void MakeCall() 
+        {
+            Console.WriteLine("Making Call..");
+        }
+        public void  HangUp() 
+        {
+            Console.WriteLine("Ending Call..");
+        }
+
+        public override void Connect() { }
+        public override void Disconnect() { }
+
+    }
+
+    public class PhoneBooth : PushButtonPhone
+    {
+        private bool superMan;
+        public double costPerCall;
+        public bool phoneBook;
+
+        public void OpenDoor() 
+        {
+            Console.WriteLine("Opening Door.");
+        }
+        public void CloseDoor() { }
+    }
+}
