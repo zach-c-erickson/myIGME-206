ea1a2d1398108229aba8f1299dcd02f57b4b8dec
zach-c-erickson <zce1382@g.rit.edu>
2021-11-24 21:51:40 -0500

    Unit Test 3 Question 7

--- Unit_Test_3_Question_7/Program.cs
+++ Unit_Test_3_Question_7/Program.cs
@@ -4,9 +4,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
+
+// Author: Zachary Erickson
+// Purpose: Unit Test 3 Question 7
+
 namespace Unit_Test_3_Question_7
 {
-
+    // Class: Wizard
+    // Purpose: Has a name and age
     public class Wizard
     {
         private string name;
@@ -28,9 +34,13 @@ namespace Unit_Test_3_Question_7
         }
     }
 
+    // Class: Program
+    // Purpose: Creates comparison delegate method and uses it to compare Wizards
     class Program
     {
 
+        // Method: CompareWizard
+        // Purpose: Uses Wizard's ages to create the comparison for List.Sort()
         public static int CompareWizard(Wizard wiz1, Wizard wiz2)
         {
             if(wiz1.Age > wiz2.Age)
@@ -47,10 +57,15 @@ namespace Unit_Test_3_Question_7
             }
         }
 
+        // Method: Main
+        // Purpose: Creates several wizards, adds them to a wizard list, and sorts the list
         static void Main(string[] args)
         {
+
+            // Creates wizardList
             List<Wizard> wizList = new List<Wizard>();
 
+            // Create several wizards with ages and names
             Wizard w0 = new Wizard("Merlin", 16);
             Wizard w1 = new Wizard("Gandalf", 24000);
             Wizard w2 = new Wizard("Glinda", 24);
@@ -62,6 +77,8 @@ namespace Unit_Test_3_Question_7
             Wizard w8 = new Wizard("Elric", 42);
             Wizard w9 = new Wizard("Prospero", 28);
 
+
+            // Adds each wizard to the wizList
             wizList.Add(w0);
             wizList.Add(w1);
             wizList.Add(w2);
@@ -74,6 +91,7 @@ namespace Unit_Test_3_Question_7
             wizList.Add(w9);
 
 
+            // Prints the wizList before sorting
             Console.WriteLine("Unsorted Wizard List:");
 
             foreach(Wizard wiz in wizList)
@@ -82,9 +100,12 @@ namespace Unit_Test_3_Question_7
             }
 
 
+            // Uses the Comparison to sort the list
             wizList.Sort(CompareWizard);
 
             Console.WriteLine();
+
+            // Prints the Sorted wizList
             Console.WriteLine("Sorted Wizard List:");
 
             foreach (Wizard wiz in wizList)
